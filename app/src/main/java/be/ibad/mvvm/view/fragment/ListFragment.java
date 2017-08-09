c464d1ff57c43fa94db887d16f41eafc9a24e4ab
nvandamme <Nicolas.VanDamme@immoweb.be>
2017-08-09 12:04:06 +0200

    Remove Loader

--- app/src/main/java/be/ibad/mvvm/view/fragment/ListFragment.java
+++ app/src/main/java/be/ibad/mvvm/view/fragment/ListFragment.java
@@ -4,18 +4,18 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import be.ibad.mvvm.R;
-import be.ibad.mvvm.data.FoodTruckLoader;
+import be.ibad.mvvm.data.FoodTruckDataSource;
+import be.ibad.mvvm.data.FoodTruckRepository;
 import be.ibad.mvvm.databinding.FragmentListBinding;
 import be.ibad.mvvm.model.Record;
 import be.ibad.mvvm.view.adapter.FoodTruckAdapter;
@@ -28,26 +28,22 @@ import be.ibad.mvvm.view.adapter.FoodTruckAdapter;
 public class ListFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     public static final String TAG = ListFragment.class.getCanonicalName();
-    private static final int LOADER_ID = 42;
     private FoodTruckAdapter mAdapter;
     private FragmentListBinding mBinding;
+    private FoodTruckDataSource.LoadFoodTruckCallback mLoadFoodTruckCallback =
+            new FoodTruckDataSource.LoadFoodTruckCallback() {
+                @Override
+                public void onTasksLoaded(List<Record> records) {
+                    mAdapter.setData(records);
+                    showHideLoadingViews(false);
+                }
 
-    private LoaderManager.LoaderCallbacks<ArrayList<Record>> mFoodLoaderCallBak = new LoaderManager.LoaderCallbacks<ArrayList<Record>>() {
-        @Override
-        public Loader<ArrayList<Record>> onCreateLoader(int id, Bundle args) {
-            return new FoodTruckLoader(getContext());
-        }
-
-        @Override
-        public void onLoadFinished(Loader<ArrayList<Record>> loader, ArrayList<Record> data) {
-            showHideLoadingViews(false);
-            mAdapter.setData(data);
-        }
-
-        @Override
-        public void onLoaderReset(Loader<ArrayList<Record>> loader) {
-        }
-    };
+                @Override
+                public void onDataNotAvailable() {
+                    Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+                    showHideLoadingViews(false);
+                }
+            };
 
     public static ListFragment newInstance() {
         return new ListFragment();
@@ -63,7 +59,9 @@ public class ListFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false);
-        return mBinding.getRoot();    }
+
+        return mBinding.getRoot();
+    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -75,7 +73,7 @@ public class ListFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         mBinding.recyclerView.hasFixedSize();
         mBinding.recyclerView.setAdapter(mAdapter);
 
-        getLoaderManager().initLoader(LOADER_ID, null, mFoodLoaderCallBak);
+        onRefresh();
     }
 
     private void showHideLoadingViews(boolean show) {
@@ -86,6 +84,8 @@ public class ListFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     @Override
     public void onRefresh() {
         showHideLoadingViews(true);
-        getLoaderManager().initLoader(LOADER_ID, null, mFoodLoaderCallBak);
+        FoodTruckRepository
+                .getInstance()
+                .getAllFoodTruck(mLoadFoodTruckCallback);
     }
 }
