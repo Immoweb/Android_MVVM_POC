91f06026ae0f975ba2e000ab8c51b71e44e12121
nvandamme <Nicolas.VanDamme@immoweb.be>
2017-08-09 12:04:02 +0200

    Improve Unit Test

---
+++ app/src/androidTest/java/be/ibad/mvvm/MockFoodTruckService.java
@@ -0,0 +1,50 @@
+package be.ibad.mvvm;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import be.ibad.mvvm.data.FoodTruckService;
+import be.ibad.mvvm.model.Record;
+import be.ibad.mvvm.model.ResponseOpenData;
+import retrofit2.Call;
+import retrofit2.mock.BehaviorDelegate;
+import retrofit2.mock.Calls;
+
+/**
+ * Created by nvandamme on 08-08-17.
+ * All right reserved for Immoweb Android_MVVM_POC
+ */
+
+public class MockFoodTruckService implements FoodTruckService {
+
+    private final BehaviorDelegate<FoodTruckService> delegate;
+    private final Context context;
+
+    public MockFoodTruckService(BehaviorDelegate<FoodTruckService> service, Context context) {
+        this.delegate = service;
+        this.context = context;
+    }
+
+    @Override
+    public Call<ResponseOpenData> getAllFoodTruck(int nbr) {
+        Gson gson = new GsonBuilder().create();
+        String json = null;
+        try {
+            json = RestServiceTestHelper.getStringFromFile(context, "response.json");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Calls.failure((IOException) e);
+        }
+        ResponseOpenData responseOpenData = gson.fromJson(json, ResponseOpenData.class);
+        responseOpenData.setNhits(nbr);
+        ArrayList<Record> records = new ArrayList<>();
+        responseOpenData.setRecords(records);
+        return delegate.returningResponse(responseOpenData).getAllFoodTruck(nbr);
+    }
+
+}
---
+++ app/src/androidTest/java/be/ibad/mvvm/RecordViewModelTest.java
@@ -0,0 +1,60 @@
+package be.ibad.mvvm;
+
+import android.test.InstrumentationTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Assert;
+
+import be.ibad.mvvm.data.FoodTruckService;
+import be.ibad.mvvm.model.ResponseOpenData;
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.mock.BehaviorDelegate;
+import retrofit2.mock.MockRetrofit;
+import retrofit2.mock.NetworkBehavior;
+
+/**
+ * Created by nvandamme on 13-07-17.
+ * All right reserved for Immoweb MVVM_POC
+ */
+
+public class RecordViewModelTest extends InstrumentationTestCase {
+    private MockRetrofit mockRetrofit;
+    private Retrofit retrofit;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://test.com")
+                .client(new OkHttpClient())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        NetworkBehavior behavior = NetworkBehavior.create();
+        mockRetrofit = new MockRetrofit.Builder(retrofit)
+                .networkBehavior(behavior)
+                .build();
+    }
+
+    @SmallTest
+    public void testNbrHitsRetrieval() throws Exception {
+
+        BehaviorDelegate<FoodTruckService> delegate = mockRetrofit.create(FoodTruckService.class);
+        FoodTruckService mockQodService = new MockFoodTruckService(delegate, getInstrumentation().getContext());
+
+        //Actual Test
+        Call<ResponseOpenData> quote = mockQodService.getAllFoodTruck(100);
+        Response<ResponseOpenData> openDataResponse = quote.execute();
+
+        //Asserting response
+        Assert.assertTrue(openDataResponse.isSuccessful());
+        Assert.assertTrue(openDataResponse.body().getRecords() != null);
+        Assert.assertEquals(100, openDataResponse.body().getNhits());
+
+    }
+
+
+}
